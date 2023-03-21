061e03996b6c12e32919ea6ad71df282ade66ad9
Evelin Ferraz <evelin.moitinho@capgemini.com>
2023-03-21 12:32:41 -0300

    [04] - Estilizando o display

--- calculadora/src/br/com/calc/view/Display.java
+++ calculadora/src/br/com/calc/view/Display.java
@@ -1,13 +1,24 @@
 package br.com.calc.view;
 
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Display extends JPanel {
 
+  //Define o texto dentro do botão
+  private JLabel numeros;
   private final Color BG_PADRAO = new Color(72, 70, 70);
 
   public Display() {
     setBackground(BG_PADRAO);
+    numeros = new JLabel("123");
+    numeros.setForeground(Color.WHITE);
+    numeros.setFont(new Font("courier", Font.PLAIN, 35));
+
+    setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 15)); //alinhamento de texto com o FlowLayout
+    add(numeros);
   }
 }
